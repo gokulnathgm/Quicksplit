cec85c44289705b96899df52be52cba956633b58
Anil Kumar <mailmeatanil94@gmail.com>
2015-12-24 12:20:32 +0530

    Add expense ui modified

--- app/src/main/java/in/inocular/www/quicksplit/NewExpense.java
+++ app/src/main/java/in/inocular/www/quicksplit/NewExpense.java
@@ -1,21 +1,29 @@
 package in.inocular.www.quicksplit;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
-import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.text.InputType;
 import android.util.Log;
-import android.view.LayoutInflater;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 
 public class NewExpense extends Activity implements View.OnClickListener {
@@ -26,12 +34,13 @@ public class NewExpense extends Activity implements View.OnClickListener {
     int[][] expense = new int[5][5];
     SharedPreferences prefs;
     String members,gid,uid,paid_owe="";
-    int l;
+    int l,temp[];
 
     Bundle extras;
     int grpId;
+    Spinner spinner;
     String grpName;
-
+    String[] k;
 
 
     @Override
@@ -46,11 +55,114 @@ public class NewExpense extends Activity implements View.OnClickListener {
         listView.setAdapter(adapter);*/
 
         addExpense = (Button) findViewById(R.id.addExpenseButton);
-        paidBy = (Button) findViewById(R.id.paidBy);
+//        paidBy = (Button) findViewById(R.id.paidBy);
         owings = (Button) findViewById(R.id.owings);
 
         title = (EditText) findViewById(R.id.title);
         total = (EditText) findViewById(R.id.total);
+
+        prefs = getSharedPreferences("file", 0);
+        members = prefs.getString("group_members", "");
+        uid = prefs.getString("members_id", "");
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+
+        //ArrayAdapter<String> adapter = ArrayAdapter.createFromResource(this, R.array.object_array, android.R.layout.simple_spinner_item);
+
+        members = members + "Multiple_People...";
+        k = members.split(" ");
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,k);
+        spinner.setAdapter(adapter);
+
+        temp = new int[k.length-1];
+        for (int i=0;i<k.length-1;i++) {
+            expense[0][i] = 0;
+            expense[1][i] = 0;
+        }
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @TargetApi(Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position == k.length-1) {
+                    android.support.v7.app.AlertDialog.Builder builder =
+                            new android.support.v7.app.AlertDialog.Builder(context, R.style.AppCompatAlertDialogStyle);
+
+                    Context context = getApplicationContext();
+                    LinearLayout layout = new LinearLayout(context);
+                    layout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                    layout.setOrientation(LinearLayout.VERTICAL);
+                    layout.setPadding(20, 5, 20, 5);
+
+                    for (int i = 0; i < k.length - 1; i++) {
+                        LinearLayout subLayout1 = new LinearLayout(context);
+                        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                        layoutParams.weight = 1;
+                        subLayout1.setLayoutParams(layoutParams);
+                        subLayout1.setGravity(Gravity.CENTER_VERTICAL);
+
+
+                        final TextView t1 = new TextView(context);
+                        t1.setText(k[i]);
+                        t1.setTextColor(Color.parseColor("#C5070607"));
+                        t1.setLayoutParams(new LinearLayout.LayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, 0.30f)));
+                        t1.setTextSize(20);
+
+                        //t1.setGravity(Gravity.BOTTOM);
+                        subLayout1.addView(t1);
+
+
+                        final EditText text1 = new EditText(context);
+                        text1.setGravity(Gravity.TOP);
+                        text1.setId(i);
+                        text1.getBackground().setColorFilter(Color.parseColor("#449094"), PorterDuff.Mode.SRC_ATOP);
+                        text1.setTextColor(Color.parseColor("#C5070607"));
+                        text1.setInputType(InputType.TYPE_CLASS_NUMBER);
+                        text1.setText(expense[0][i]+"");
+                        text1.setLayoutParams(new LinearLayout.LayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, 0.70f)));
+
+                        subLayout1.addView(text1);
+                        layout.addView(subLayout1);
+                    }
+                    builder.setView(layout);
+
+                    builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            Dialog f = (Dialog) dialog;
+                            for (int m = 0; m < k.length - 1; m++) {
+
+                                EditText text = (EditText) f.findViewById(m);
+                                expense[0][m] = Integer.parseInt(text.getText().toString());
+                            }
+                        }
+                    });
+                    builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+                    builder.setTitle("Multiple people paid");
+
+                    android.support.v7.app.AlertDialog dialog = builder.create();
+                    dialog.show();
+                    Window window = dialog.getWindow();
+                    window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
+                    int width = (int) (getResources().getDisplayMetrics().widthPixels * 0.90);
+                    window.setLayout(width, LinearLayout.LayoutParams.WRAP_CONTENT);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+
+        });
 
         extras = getIntent().getExtras();
         grpId = extras.getInt("Group_Id");
@@ -65,7 +177,7 @@ public class NewExpense extends Activity implements View.OnClickListener {
      //   Log.d("members id",uid);
 
         addExpense.setOnClickListener(this);
-        paidBy.setOnClickListener(this);
+//        paidBy.setOnClickListener(this);
         owings.setOnClickListener(this);
 
         //Toast.makeText(getApplicationContext(),grpId+"",Toast.LENGTH_SHORT).show();
@@ -75,105 +187,112 @@ public class NewExpense extends Activity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
 
-        prefs = getSharedPreferences("file", 0);
-        members = prefs.getString("group_members", "");
-        uid = prefs.getString("members_id", "");
-
         switch (v.getId()) {
-            case R.id.addExpenseButton:
-                String expensetitle = title.getText().toString();
-                String totalexpense = total.getText().toString();
-                for(int i=0;i<l;i++){
-                    paid_owe+=String.valueOf(expense[0][i])+" ";
-                    paid_owe+=String.valueOf(expense[1][i])+" ";
-                }
-                Log.d("paid-owe",paid_owe);
-                new AddExpense(NewExpense.this,grpName).execute(gid,expensetitle,totalexpense,grpName,uid,paid_owe);
-                /*Intent i = new Intent(this, GroupActivity.class);
-                i.putExtra("Group_Id",grpId);
-                i.putExtra("Group_Name",grpName);
-                startActivity(i);*/
-                break;
-            case R.id.paidBy:
-                displayPaymentPopup();
-                break;
             case R.id.owings:
                 displayOwingsPopup();
+                break;
+            case R.id.addExpenseButton:
+                add_expense();
                 break;
         }
     }
 
+    private void add_expense() {
+        int pos = spinner.getSelectedItemPosition();
+        String totalexpense = total.getText().toString();
+        String expensetitle = title.getText().toString();
+
+
+        if (pos != k.length - 1) {
+            for (int i=0;i<k.length-1;i++) {
+                expense[0][i] = 0;
+            }
+            expense[0][pos] = Integer.parseInt(totalexpense);
+        }
+        for(int i=0;i<k.length-1;i++) {
+            Log.d("",expense[0][i] + " ---------" + expense[1][i]+"\n");
+        }
+        //Intent i = new Intent(this, GroupActivity.class);
+        //startActivity(i);
+
+
+        //new AddExpense(NewExpense.this).execute(gid,expensetitle,totalexpense,grpName);
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
     private void displayOwingsPopup() {
-        LayoutInflater inflater = LayoutInflater.from(context);
-        View popUpView = inflater.inflate(R.layout.payment_popup, null);
 
-        final ListView listView = (ListView) popUpView.findViewById(R.id.listView);
-        String[] k = members.split(" ");
-        l = k.length;
-        String[] valuesForListView = k;//{"Anil owes ", "Gokul owes", "Sai owes", "Divya owes", "Dhrisya owes"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.list_view_for_popup,R.id.nameOfGroupMember,valuesForListView);
-        listView.setAdapter(adapter);
+        AlertDialog.Builder builder = new AlertDialog.Builder(context, R.style.AppCompatAlertDialogStyle);
+
+        Context context = getApplicationContext();
+        LinearLayout layout = new LinearLayout(context);
+        layout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.setPadding(20, 5, 20, 5);
+
+        for (int i = 0; i < k.length - 1; i++) {
+            LinearLayout subLayout1 = new LinearLayout(context);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            layoutParams.weight = 1;
+            subLayout1.setLayoutParams(layoutParams);
+            subLayout1.setGravity(Gravity.CENTER_VERTICAL);
 
 
+            final TextView t1 = new TextView(context);
+            t1.setText(k[i]);
+            t1.setTextColor(Color.parseColor("#C5070607"));
+            t1.setLayoutParams(new LinearLayout.LayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, 0.30f)));
+            t1.setTextSize(20);
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        builder.setView(popUpView);
+            //t1.setGravity(Gravity.BOTTOM);
+            subLayout1.addView(t1);
+
+
+            final EditText text1 = new EditText(context);
+            text1.setGravity(Gravity.TOP);
+            text1.setId(i);
+            text1.getBackground().setColorFilter(Color.parseColor("#449094"), PorterDuff.Mode.SRC_ATOP);
+            text1.setTextColor(Color.parseColor("#C5070607"));
+            text1.setInputType(InputType.TYPE_CLASS_NUMBER);
+            text1.setText(expense[1][i]+"");
+            text1.setLayoutParams(new LinearLayout.LayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT, 0.70f)));
+
+            subLayout1.addView(text1);
+            layout.addView(subLayout1);
+        }
+        builder.setView(layout);
+
         builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+
             @Override
             public void onClick(DialogInterface dialog, int which) {
 
-                for (int i = 0; i < l; i++) {
-                    View view = listView.getChildAt(i);
-                    EditText text = (EditText) view.findViewById(R.id.value);
-                    expense[1][i] = Integer.parseInt(text.getText().toString());
+                Dialog f = (Dialog) dialog;
+                for (int m = 0; m < k.length - 1; m++) {
+
+                    EditText text = (EditText) f.findViewById(m);
+                    expense[1][m] = Integer.parseInt(text.getText().toString());
                 }
             }
         });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
 
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
 
+            }
+        });
+        builder.setTitle("Enter each person's share");
 
         AlertDialog dialog = builder.create();
         dialog.show();
         Window window = dialog.getWindow();
-        window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE| WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
-        int width = (int) (getResources().getDisplayMetrics().widthPixels*0.80);
-        window.setLayout(width, LinearLayout.LayoutParams.WRAP_CONTENT);
-
-
-    }
-
-    private void displayPaymentPopup() {
-        LayoutInflater inflater = LayoutInflater.from(context);
-        View popUpView = inflater.inflate(R.layout.payment_popup, null);
-
-
-        final ListView listView = (ListView) popUpView.findViewById(R.id.listView);
-        String[] k = members.split(" ");
-        l = k.length;
-        String[] valuesForListView = k;//{"Anil paid ", "Gokul paid", "Sai paid", "Divya paid", "Dhrisya paid"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.list_view_for_popup,R.id.nameOfGroupMember,valuesForListView);
-        listView.setAdapter(adapter);
-
-
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
-        builder.setView(popUpView);
-        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                for (int i = 0; i < l; i++) {
-                    View view = listView.getChildAt(i);
-                    EditText text = (EditText) view.findViewById(R.id.value);
-                    expense[0][i] = Integer.parseInt(text.getText().toString());
-                }
-            }
-        });
-                AlertDialog dialog = builder.create();
-        dialog.show();
-        Window window = dialog.getWindow();
         window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_ALT_FOCUSABLE_IM);
-        int width = (int) (getResources().getDisplayMetrics().widthPixels*0.80);
+        int width = (int) (getResources().getDisplayMetrics().widthPixels * 0.90);
         window.setLayout(width, LinearLayout.LayoutParams.WRAP_CONTENT);
+
     }
+
 
     @Override
     protected void onPause() {
